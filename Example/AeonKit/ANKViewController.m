f520e3d78436ab42ef2b4e7d7f346f8a6baf5334
Akira Matsuda <akira.matsuda@me.com>
2016-02-09 07:00:05 +0900

    Initial commit

---
+++ Example/AeonKit/ANKViewController.m
@@ -0,0 +1,29 @@
+//
+//  ANKViewController.m
+//  AeonKit
+//
+//  Created by Akira Matsuda on 02/09/2016.
+//  Copyright (c) 2016 Akira Matsuda. All rights reserved.
+//
+
+#import "ANKViewController.h"
+
+@interface ANKViewController ()
+
+@end
+
+@implementation ANKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
